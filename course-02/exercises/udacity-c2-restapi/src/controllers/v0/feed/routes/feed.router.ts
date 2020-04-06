9a250c26bb7b3321654a2ca5b04c56256b021c97
arampall <achyut.9316@gmail.com>
2020-04-06 16:44:30 -0700

    Added TODO

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
@@ -16,15 +16,56 @@ router.get('/', async (req: Request, res: Response) => {
     res.send(items);
 });
 
-//@TODO
 //Add an endpoint to GET a specific resource by Primary Key
+router.get('/:id', async(req: Request, res: Response) => {
+    let {id} = req.params;
+    console.log('id is ', id);
+    if(!id){
+        res.status(400).send("Id needs to be provided to process the request.");
+    }
+
+    try{
+        const item = await FeedItem.findByPk(id);
+        if(!item){
+            res.status(404).send("Cannot find a feed with id = " + id);
+        }
+        res.status(200).send(item);
+    }
+    catch(e){
+        console.error(e);
+        res.status(500).send('Unable to handle the request.');
+    }
+      
+});
 
 // update a specific resource
 router.patch('/:id', 
     requireAuth, 
     async (req: Request, res: Response) => {
-        //@TODO try it yourself
-        res.send(500).send("not implemented")
+        let {id} = req.params
+        let patch = req.body
+        console.log('id is ', id);
+        console.log('patch is', patch);
+        if(!id){
+            res.status(400).send("Id needs to be provided to process the request.");
+        }
+    
+        try{
+            let item = await FeedItem.findByPk(id);
+            if(!item){
+                res.status(404).send("Cannot find a feed with id = " + id);
+            }
+            for( let key in patch){
+                item.set(key, patch[key]) ;
+            }
+            item = await item.save();
+
+            res.status(200).send(item);
+        }
+        catch(e){
+            console.error(e);
+            res.status(500).send('Unable to handle the request.');
+        }
 });
 
 
